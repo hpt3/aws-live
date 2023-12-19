3826520eb12755946c588d6369c99f6eeb1e5ab7
hpt3 <154297454+hpt3@users.noreply.github.com>
2023-12-19 12:08:20 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ customhost = "database-1.czmi0w04kqil.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "hiipuongtih-employee"
+custombucket = "hiipuongtih-bucket"
 customregion = "us-east-1"
 
